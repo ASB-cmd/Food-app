c2221d08b9215c0d7e2652a5b8763e8a94c937ee
Akshay Bhope <akshay.bhope@sjsu.edu>
2025-04-17 22:48:56 -0700

    Header Updated

--- app/meals/community/page.js
+++
@@ -1,8 +0,0 @@
-export default function Community({ params }) {
-  return (
-    <div>
-      <h1>Communty</h1>
-      <p>Slug: {params.slug}</p>
-    </div>
-  );
-}
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,23 @@
 import Link from "next/link";
+import classes from "./page.module.css";
+import Mealsgrid from "@/components/meals/meals.grid";
 
-export default function MealsPage() {
+export default function SharePage() {
   return (
-    <div>
-      <h1>Meals Page</h1>
-      <Link href="/meals/meal-1">Meal1</Link>
-      <Link href="/meals/meal-2">Meal2</Link>
-    </div>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.heighlight}>by you</span>
+        </h1>
+        <p>Choose your favourite recipes and cooki it yourself</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favourite receipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Mealsgrid meals ={[]}/>
+      </main>
+    </>
   );
 }
